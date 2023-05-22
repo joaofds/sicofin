9a0b3529966ad3a9076e46e93a0ab8cc0948b40f
Joao <jo4o.fds@gmail.com>
2023-05-21 23:17:39 -0300

    Menu para rotinas de cadastro

--- Core/Helpers.cs
+++ Core/Helpers.cs
@@ -6,6 +6,24 @@ namespace Core
     public class Helpers
     {
         /* 
+        * Monta menu dinamicamente.
+        *
+        */
+        public static void MakeMenu(string usuario, string menu, string mensagem)
+        {
+            int len = mensagem.Length;
+            int totalChars = 62 - len;
+            string chars = string.Concat(Enumerable.Repeat(" ", totalChars));
+
+            Console.Clear();
+            Console.WriteLine($"{menu} | Olá {usuario}, Seja Bem Vindo!");
+            Console.WriteLine("");
+            Console.WriteLine("+---------------------------------------------------------------+");
+            Console.WriteLine($"| {mensagem}{chars}|");
+            Console.WriteLine("+---------------------------------------------------------------+");
+        }
+
+        /* 
         * Remove caracteres inválidos de uma string.
         *
         */
